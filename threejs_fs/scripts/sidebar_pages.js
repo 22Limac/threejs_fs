7ad9137d7f196411eb54ef22a4e686a2c47e5f89
22Limac <22limac@gmail.com>
2022-03-30 20:02:01 -0300

    Added a responsive vertical navbar to my site

--- threejs_fs/scripts/sidebar_pages.js
+++ threejs_fs/scripts/sidebar_pages.js
@@ -1,9 +1,26 @@
 import "../style.css";
 
-const article_link = document.getElementById("article");
 
-const displayArticle = () => {
-    document.getElementById("article-page").style.display = "block";
-}
+// const articleLink = document.getElementById("article");
+// const articlePage = document.getElementById("article-page");
 
-article_link.addEventListener("click", displayArticle);
+// const displayArticle = () => {
+//   articlePage.className = "show";
+// };
+
+// articleLink.addEventListener("mouseover", displayArticle);
+
+// const hideArticle = () => {
+//     articlePage.className = "hide";
+// };
+
+// articlePage.addEventListener("mouseleave", hideArticle);
+
+
+const route = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+};
+
+window.route = route;
